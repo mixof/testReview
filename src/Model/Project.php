f20b316fa5f50f23a90e9ce0fffbbb020fa716a1
Aleksey Ilyuchenko <mixof777@gmail.com>
2022-10-11 11:13:57 +0300

    Project Model review

--- src/Model/Project.php
+++ src/Model/Project.php
@@ -8,7 +8,8 @@ class Project
      * @var array
      */
     public $_data;
-    
+
+	//Need to add param type because now in the $data can be any object, but we expected that there are an array
     public function __construct($data)
     {
         $this->_data = $data;
@@ -19,6 +20,7 @@ class Project
      */
     public function getId()
     {
+		//Need to check if value by key "id" exists
         return (int) $this->_data['id'];
     }
 
